ade682b5fc0460453a76023e358df5e23b182885
Gyuho Lee <leegyuho@amazon.com>
2018-10-15 01:11:14 -0700

    scripts: add "kubekins-e2e" build scripts
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

---
+++ scripts/kubekins-e2e.build.push.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+if ! [[ "$0" =~ scripts/kubekins-e2e.build.push.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+$(aws ecr get-login --no-include-email --region us-west-2)
+
+if [[ -z "${GIT_COMMIT}" ]]; then
+  GIT_COMMIT=$(git rev-parse --short=12 HEAD || echo "GitNotFound")
+fi
+
+_AWS_REGION=us-west-2
+if [[ "${AWS_REGION}" ]]; then
+  _AWS_REGION=${AWS_REGION}
+fi
+
+if [[ -z "${REGISTRY}" ]]; then
+  REGISTRY=$(awstester ecr --region=${_AWS_REGION} get-registry)
+fi
+
+echo "Building:" ${REGISTRY}/kubekins-e2e:${GIT_COMMIT}
+
+docker build \
+  --tag ${REGISTRY}/kubekins-e2e:${GIT_COMMIT} \
+  --file ./scripts/Dockerfile .
+
+docker push ${REGISTRY}/kubekins-e2e:${GIT_COMMIT}
