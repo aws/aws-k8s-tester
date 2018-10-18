152bb09d45b79d418e9069fbf86d3452fd027589
Gyuho Lee <leegyuho@amazon.com>
2018-10-17 21:57:09 -0700

    internal/eks/alb: test target health
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- internal/eks/alb/test.go
+++ internal/eks/alb/test.go
@@ -13,6 +13,7 @@ func (md *embedded) TestAWSResources() error {
 		"testing ALB resources",
 		zap.Int("elbv2-number", len(md.cfg.ALBIngressController.ELBv2NameToARN)),
 	)
+
 	for name, arn := range md.cfg.ALBIngressController.ELBv2NameToARN {
 		desc, err := md.elbv2.DescribeTargetGroups(&elbv2.DescribeTargetGroupsInput{
 			LoadBalancerArn: aws.String(arn),
@@ -32,7 +33,38 @@ func (md *embedded) TestAWSResources() error {
 		if n == 0 {
 			return fmt.Errorf("found no target groups from %q", name)
 		}
+
+		for _, tg := range desc.TargetGroups {
+			var healthOut *elbv2.DescribeTargetHealthOutput
+			healthOut, err = md.elbv2.DescribeTargetHealth(&elbv2.DescribeTargetHealthInput{
+				TargetGroupArn: tg.TargetGroupArn,
+			})
+			if err != nil {
+				md.lg.Warn(
+					"failed to describe target group health",
+					zap.String("elbv2-name", name),
+					zap.Error(err),
+				)
+				return err
+			}
+			ta := *tg.TargetGroupArn
+			for _, hv := range healthOut.TargetHealthDescriptions {
+				hs := *hv.TargetHealth.State
+				if hs != "healthy" {
+					md.lg.Warn(
+						"found unhealthy target",
+						zap.String("elbv2-name", name),
+						zap.String("elbv2-target-group-arn", ta),
+						zap.String("target-type", *tg.TargetType),
+						zap.Int64("target-port", *hv.Target.Port),
+						zap.String("target-state", hs),
+					)
+					return fmt.Errorf("found unhealthy target with state %q (target group ARN %q)", hs, ta)
+				}
+			}
+		}
 	}
+
 	md.lg.Info(
 		"tested ALB resources",
 		zap.Int("elbv2-number", len(md.cfg.ALBIngressController.ELBv2NameToARN)),
