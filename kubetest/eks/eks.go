054293584199cec1de666b83a1a164a97ee7a431
Gyuho Lee <leegyuho@amazon.com>
2018-11-11 00:16:17 -0800

    kubetest/eks: sync with upstream
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- kubetest/eks/eks.go
+++ kubetest/eks/eks.go
@@ -15,9 +15,8 @@ limitations under the License.
 */
 
 // Package eks implements 'kubetest' deployer interface.
-// It uses 'aws-k8s-tester' and 'kubectl' binaries, rather than importing
-// EKS packages directly. This is to eliminate the need of dependency
-// management, both in upstream and downstream.
+// It uses 'aws-k8s-tester' and 'kubectl' binaries, rather than importing internal packages.
+// All underlying implementation and external dependencies are compiled into one binary.
 package eks
 
 import (
@@ -46,7 +45,6 @@ type deployer struct {
 	stopc            chan struct{}
 	cfg              *eksconfig.Config
 	awsK8sTesterPath string
-	kubectlPath      string
 	ctrl             *process.Control
 }
 
@@ -85,54 +83,28 @@ func NewDeployer(timeout time.Duration, verbose bool) (ekstester.Deployer, error
 
 	dp.awsK8sTesterPath, err = exec.LookPath("aws-k8s-tester")
 	if err != nil {
-		dp.awsK8sTesterPath = filepath.Join(os.TempDir(), "aws-k8s-tester")
 		var f *os.File
-		f, err = os.Create(dp.awsK8sTesterPath)
+		f, err = ioutil.TempFile(os.TempDir(), "aws-k8s-tester")
 		if err != nil {
 			return nil, fmt.Errorf("failed to create %q (%v)", dp.awsK8sTesterPath, err)
 		}
-		defer f.Close()
+		dp.awsK8sTesterPath = f.Name()
+		dp.awsK8sTesterPath, _ = filepath.Abs(dp.awsK8sTesterPath)
 		if err = httpRead(cfg.AWSK8sTesterDownloadURL, f); err != nil {
 			return nil, err
 		}
+		f.Close()
 		if err = util.EnsureExecutable(dp.awsK8sTesterPath); err != nil {
 			return nil, err
 		}
 	}
-
-	dp.kubectlPath, err = exec.LookPath("kubectl")
-	if err != nil {
-		return nil, fmt.Errorf("cannot find 'kubectl' executable (%v)", err)
-	}
-
-	// TODO(gyuho): replace this kubernetes native Go client
-	_, err = exec.LookPath("aws-iam-authenticator")
-	if err != nil {
-		bin := filepath.Join(os.TempDir(), "aws-iam-authenticator")
-		var f *os.File
-		f, err = os.Create(bin)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create %q (%v)", bin, err)
-		}
-		defer f.Close()
-		if err = httpRead(cfg.AWSIAMAuthenticatorDownloadURL, f); err != nil {
-			return nil, err
-		}
-		if err = util.EnsureExecutable(bin); err != nil {
-			return nil, err
-		}
-		if err = os.Rename(bin, "/usr/local/bin/aws-iam-authenticator"); err != nil {
-			return nil, err
-		}
-	}
-
 	return dp, nil
 }
 
 // Up creates a new EKS cluster.
 func (dp *deployer) Up() (err error) {
 	// "create cluster" command outputs cluster information
-	// in the configuraion file (e.g. VPC ID, ALB DNS names, etc.)
+	// in the configuration file (e.g. VPC ID, ALB DNS names, etc.)
 	// this needs be reloaded for other deployer method calls
 	createCmd := exec.Command(
 		dp.awsK8sTesterPath,
@@ -257,7 +229,7 @@ func (dp *deployer) DumpClusterLogs(artifactDir, _ string) (err error) {
 
 // Stop stops ongoing operations.
 // This is useful for local development.
-// For example, one may run "Up" but have to cancel onging "Up"
+// For example, one may run "Up" but have to cancel ongoing "Up"
 // operation. Then, it can just send syscall.SIGINT to trigger "Stop".
 func (dp *deployer) Stop() {
 	close(dp.stopc)
