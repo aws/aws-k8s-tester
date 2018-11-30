989b95d4f9ea02f17cd7bbafe0211180e10d2d12
Gyuho Lee <leegyuho@amazon.com>
2018-11-30 11:19:27 -0800

    eksconfig: use 0.1.3 as default "AWSK8sTesterDownloadURL"
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- eksconfig/config.go
+++ eksconfig/config.go
@@ -397,7 +397,7 @@ func genTag() string {
 var defaultConfig = Config{
 	TestMode: "embedded",
 
-	AWSK8sTesterDownloadURL:        "https://github.com/aws/aws-k8s-tester/releases/download/0.1.2/aws-k8s-tester-0.1.2-linux-amd64",
+	AWSK8sTesterDownloadURL:        "https://github.com/aws/aws-k8s-tester/releases/download/0.1.3/aws-k8s-tester-0.1.3-linux-amd64",
 	AWSK8sTesterPath:               "/tmp/aws-k8s-tester/aws-k8s-tester",
 	KubectlDownloadURL:             "https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/kubectl",
 	KubectlPath:                    "/tmp/aws-k8s-tester/kubectl",
