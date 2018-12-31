ac6aa09ff106e271614f3d3ba86f4a7ec7e6c1f3
Kirsten Schumy <ksschumy@amazon.com>
2018-12-30 18:55:09 -0800

    internal/csi: add github account to csi.go

--- internal/csi/csi.go
+++ internal/csi/csi.go
@@ -50,7 +50,7 @@ func NewTester(cfg *ec2config.Config, terminateOnExit, journalctlLogs bool) (tes
 	return tester, nil
 }
 
-func CreateConfig(vpcID, branchOrPR string) *ec2config.Config {
+func CreateConfig(vpcID, branchOrPR, githubAccount string) *ec2config.Config {
 	cfg := ec2config.NewDefault()
 	cfg.UploadTesterLogs = false
 	cfg.VPCID = vpcID
@@ -58,7 +58,7 @@ func CreateConfig(vpcID, branchOrPR string) *ec2config.Config {
 	cfg.Plugins = []string{
 		"update-amazon-linux-2",
 		"install-go-1.11.3",
-		"install-csi-" + branchOrPR,
+		"install-csi-" + branchOrPR + "/github-account-" + githubAccount,
 	}
 	cfg.Wait = true
 	return cfg
