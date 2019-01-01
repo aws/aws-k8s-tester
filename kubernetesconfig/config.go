65953484735dedff9bf71d3a99ed68f5093f1c2f
Gyuho Lee <leegyuho@amazon.com>
2018-12-31 16:21:03 -0800

    kubernetesconfig: set defaults for kubelet
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- kubernetesconfig/config.go
+++ kubernetesconfig/config.go
@@ -131,10 +131,29 @@ type Kubelet struct {
 	DownloadURL    string `json:"download-url"`
 	VersionCommand string `json:"version-command"`
 
-	AllowPrivileged bool   `json:"allow-privileged" kubelet:"allow-privileged"`
-	AnonymousAuth   bool   `json:"anonymous-auth" kubelet:"anonymous-auth"`
-	CgroupRoot      string `json:"cgroup-root" kubelet:"cgroup-root"`
-	PodManifestPath string `json:"pod-manifest-path" kubelet:"pod-manifest-path"`
+	AllowPrivileged         bool   `json:"allow-privileged" kubelet:"allow-privileged"`
+	AnonymousAuth           bool   `json:"anonymous-auth" kubelet:"anonymous-auth"`
+	CgroupRoot              string `json:"cgroup-root" kubelet:"cgroup-root"`
+	ClientCAFile            string `json:"client-ca-file" kubelet:"client-ca-file"`
+	CloudProvider           string `json:"cloud-provider" kubelet:"cloud-provider"`
+	ClusterDNS              string `json:"cluster-dns" kubelet:"cluster-dns"`
+	ClusterDomain           string `json:"cluster-domain" kubelet:"cluster-domain"`
+	EnableDebuggingHandlers bool   `json:"enable-debugging-handlers" kubelet:"enable-debugging-handlers"`
+	EvictionHard            string `json:"eviction-hard" kubelet:"eviction-hard"`
+	FeatureGates            string `json:"feature-gates" kubelet:"feature-gates"`
+	HostnameOverride        string `json:"hostname-override" kubelet:"hostname-override"`
+	Kubeconfig              string `json:"kubeconfig" kubelet:"kubeconfig"`
+	NetworkPluginMTU        int64  `json:"network-plugin-mtu" kubelet:"network-plugin-mtu"`
+	NetworkPlugin           string `json:"network-plugin" kubelet:"network-plugin"`
+	NodeLabels              string `json:"node-labels" kubelet:"node-labels"`
+	NonMasqueradeCIDR       string `json:"non-masquerade-cidr" kubelet:"non-masquerade-cidr"`
+	PodInfraContainerImage  string `json:"pod-infra-container-image" kubelet:"pod-infra-container-image"`
+	PodManifestPath         string `json:"pod-manifest-path" kubelet:"pod-manifest-path"`
+	RegisterSchedulable     bool   `json:"register-schedulable" kubelet:"register-schedulable"`
+	RegisterWithTaints      string `json:"register-with-taints" kubelet:"register-with-taints"`
+	V                       int    `json:"v" kubelet:"v"`
+	CNIBinDir               string `json:"cni-bin-dir" kubelet:"cni-bin-dir"`
+	CNIConfDir              string `json:"cni-conf-dir" kubelet:"cni-conf-dir"`
 }
 
 type KubeProxy struct {
@@ -267,20 +286,57 @@ var defaultConfig = Config{
 		DownloadURL:    "https://storage.googleapis.com/kubernetes-release/release/v1.13.1/bin/linux/amd64/kubelet",
 		VersionCommand: "/usr/bin/kubelet --version",
 
-		AllowPrivileged: true,
-		AnonymousAuth:   false,
-		CgroupRoot:      "/",
-		PodManifestPath: "/etc/kubernetes/manifests",
+		AllowPrivileged:         true,
+		AnonymousAuth:           false,
+		CgroupRoot:              "/",
+		ClientCAFile:            "/srv/kubernetes/ca.crt",
+		CloudProvider:           "aws",
+		ClusterDNS:              "100.64.0.10", // ??
+		ClusterDomain:           "cluster.local",
+		EnableDebuggingHandlers: true,
+		EvictionHard:            "memory.available<100Mi,nodefs.available<10%,nodefs.inodesFree<5%,imagefs.available<10%,imagefs.inodesFree<5%",
+		FeatureGates:            "ExperimentalCriticalPodAnnotation=true",
+		HostnameOverride:        "PRIVATE_DNS",
+		Kubeconfig:              "/var/lib/kubelet/kubeconfig",
+		NetworkPluginMTU:        9001,
+		NetworkPlugin:           "kubenet",
+		NodeLabels:              "aws-k8s-tester.k8s.io/instancegroup=master-us-west-2a,kubernetes.io/role=master,node-role.kubernetes.io/master=",
+		NonMasqueradeCIDR:       "100.64.0.0/10",
+		PodInfraContainerImage:  "k8s.gcr.io/pause-amd64:3.0",
+		PodManifestPath:         "/etc/kubernetes/manifests",
+		RegisterSchedulable:     true,
+		RegisterWithTaints:      "node-role.kubernetes.io/master=:NoSchedule",
+		V:                       2,
+		CNIBinDir:               "/opt/cni/bin/",
+		CNIConfDir:              "/etc/cni/net.d/",
 	},
 	KubeletWorkerNodes: &Kubelet{
 		Path:           "/usr/bin/kubelet",
 		DownloadURL:    "https://storage.googleapis.com/kubernetes-release/release/v1.13.1/bin/linux/amd64/kubelet",
 		VersionCommand: "/usr/bin/kubelet --version",
 
-		AllowPrivileged: true,
-		AnonymousAuth:   false,
-		CgroupRoot:      "/",
-		PodManifestPath: "/etc/kubernetes/manifests",
+		AllowPrivileged:         true,
+		AnonymousAuth:           false,
+		CgroupRoot:              "/",
+		ClientCAFile:            "/srv/kubernetes/ca.crt",
+		CloudProvider:           "aws",
+		ClusterDNS:              "100.64.0.10", // ??
+		ClusterDomain:           "cluster.local",
+		EnableDebuggingHandlers: true,
+		EvictionHard:            "memory.available<100Mi,nodefs.available<10%,nodefs.inodesFree<5%,imagefs.available<10%,imagefs.inodesFree<5%",
+		FeatureGates:            "ExperimentalCriticalPodAnnotation=true",
+		HostnameOverride:        "PRIVATE_DNS",
+		Kubeconfig:              "/var/lib/kubelet/kubeconfig",
+		NetworkPluginMTU:        9001,
+		NetworkPlugin:           "kubenet",
+		NodeLabels:              "aws-k8s-tester.k8s.io/instancegroup=nodes,kubernetes.io/role=node,node-role.kubernetes.io/node=",
+		NonMasqueradeCIDR:       "100.64.0.0/10",
+		PodInfraContainerImage:  "k8s.gcr.io/pause-amd64:3.0",
+		PodManifestPath:         "/etc/kubernetes/manifests",
+		RegisterSchedulable:     true,
+		V:                       2,
+		CNIBinDir:               "/opt/cni/bin/",
+		CNIConfDir:              "/etc/cni/net.d/",
 	},
 
 	KubeProxyMasterNodes: &KubeProxy{
