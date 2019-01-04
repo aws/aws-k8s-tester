75ed3eed64cb97b04246c11fd7824b595355618f
Gyuho Lee <leegyuho@amazon.com>
2019-01-03 19:08:53 -0800

    internal/ec2: fix a grammatical error in logging
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- internal/ec2/security_group.go
+++ internal/ec2/security_group.go
@@ -113,7 +113,7 @@ func (md *embedded) deleteSecurityGroup() error {
 		awsErr, ok := err.(awserr.Error)
 		if ok && awsErr.Code() == "InvalidGroup.NotFound" {
 			md.lg.Info(
-				"deleted all security group",
+				"deleted all security groups",
 				zap.Strings("group-ids", md.cfg.SecurityGroupIDs),
 			)
 			return nil
