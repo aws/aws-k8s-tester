39a6fcc687a45590b594e285708d9d03287873e5
Gyuho Lee <leegyuho@amazon.com>
2019-12-05 03:39:22 -0800

    scripts/build.sh: add "arm64" builds
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -35,6 +35,15 @@ for os in linux darwin; do
     ./cmd/aws-k8s-tester
 done
 
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 \
+  go build -v \
+  -ldflags "-s -w \
+  -X github.com/aws/aws-k8s-tester/version.GitCommit=${GIT_COMMIT} \
+  -X github.com/aws/aws-k8s-tester/version.ReleaseVersion=${RELEASE_VERSION} \
+  -X github.com/aws/aws-k8s-tester/version.BuildTime=${BUILD_TIME}" \
+  -o ./bin/aws-k8s-tester-${RELEASE_VERSION}-linux-arm64 \
+  ./cmd/aws-k8s-tester
+
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
   ./bin/aws-k8s-tester-${RELEASE_VERSION}-linux-$(go env GOARCH) version
 elif [[ "${OSTYPE}" == "darwin"* ]]; then
