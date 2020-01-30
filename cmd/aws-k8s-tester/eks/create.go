aac5ef7ba33ae75239473424646aa84b61a6c329
Gyuho Lee <leegyuho@amazon.com>
2020-01-30 11:50:11 -0800

    cmd/aws-k8s-tester: add "eks create mng"
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- cmd/aws-k8s-tester/eks/create.go
+++ cmd/aws-k8s-tester/eks/create.go
@@ -18,6 +18,7 @@ func newCreate() *cobra.Command {
 	ac.AddCommand(
 		newCreateConfig(),
 		newCreateCluster(),
+		newCreateMNG(),
 	)
 	return ac
 }
@@ -110,4 +111,59 @@ func createClusterFunc(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 	fmt.Println("'aws-k8s-tester eks create cluster' success")
+}
+
+func newCreateMNG() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "mng",
+		Short: "Create/Add an EKS Managed Node Group",
+		Long:  "Configuration values are overwritten by environment variables.",
+		Run:   createMNGFunc,
+	}
+	return cmd
+}
+
+func createMNGFunc(cmd *cobra.Command, args []string) {
+	if !fileutil.Exist(path) {
+		fmt.Fprintf(os.Stderr, "cannot find configuration %q\n", path)
+		os.Exit(1)
+	}
+
+	cfg, err := eksconfig.Load(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load configuration %q (%v)\n", path, err)
+		os.Exit(1)
+	}
+
+	if err = cfg.ValidateAndSetDefaults(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to validate configuration %q (%v)\n", path, err)
+		os.Exit(1)
+	}
+	cfg.Sync()
+
+	fmt.Println("overwriting config file from environment variables")
+	err = cfg.UpdateFromEnvs()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load configuration from environment variables: %v\n", err)
+		os.Exit(1)
+	}
+	cfg.Sync()
+
+	if err = cfg.ValidateAndSetDefaults(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to validate configuration %q (%v)\n", path, err)
+		os.Exit(1)
+	}
+	cfg.Sync()
+
+	tester, err := eks.New(cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create EKS deployer %v\n", err)
+		os.Exit(1)
+	}
+
+	if err = tester.CreateMNG(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create mng %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("'aws-k8s-tester eks create mng' success")
 }
