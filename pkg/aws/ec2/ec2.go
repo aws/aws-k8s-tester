5c75c7b598449c774726ac6d32ed0409237a7242
Gyuho Lee <leegyuho@amazon.com>
2020-02-12 11:39:03 -0800

    pkg/aws/ec2: fix a bug in "ec2.DescribeInstances"
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- pkg/aws/ec2/ec2.go
+++ pkg/aws/ec2/ec2.go
@@ -36,18 +36,18 @@ func describeByBatch(
 	instanceIDs ...string) (ec2Instances map[string]*ec2.Instance, err error) {
 	ec2Instances = make(map[string]*ec2.Instance, len(instanceIDs))
 
-	ids := make([]string, len(instanceIDs))
-	copy(ids, instanceIDs)
+	last := make([]string, len(instanceIDs))
+	copy(last, instanceIDs)
 
 	// batch by 10
-	for len(ids) > 0 {
-		iss := ids
-		if len(ids) > 10 {
-			iss = ids[:10]
+	for len(last) > 0 {
+		batch := last
+		if len(last) > 10 {
+			batch = last[:10]
 		}
 		var dout *ec2.DescribeInstancesOutput
 		dout, err = ec2API.DescribeInstances(&ec2.DescribeInstancesInput{
-			InstanceIds: aws.StringSlice(iss),
+			InstanceIds: aws.StringSlice(batch),
 		})
 		if err != nil {
 			return nil, fmt.Errorf("failed to describe instances %v", err)
@@ -62,18 +62,18 @@ func describeByBatch(
 				ec2Instances[aws.StringValue(iv.InstanceId)] = iv
 			}
 		}
-		if runningCnt != len(iss) {
-			return nil, fmt.Errorf("running instances expected %d, got %d", len(iss), runningCnt)
+		if runningCnt != len(batch) {
+			return nil, fmt.Errorf("running instances expected %d, got %d", len(batch), runningCnt)
 		}
 		lg.Info("EC2 instances are running",
 			zap.Int("reservations", len(dout.Reservations)),
 			zap.Int("instances-so-far", len(ec2Instances)),
 		)
 
-		if len(ids) <= 10 {
+		if len(last) <= 10 {
 			break
 		}
-		ids = ids[:10]
+		last = last[10:]
 		time.Sleep(5 * time.Second)
 	}
 
