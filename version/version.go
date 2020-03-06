5a3ec45b5230747adfda28d22434dcef6b45430e
Gyuho Lee <leegyuho@amazon.com>
2020-03-06 12:04:48 -0800

    version: set defaults
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- version/version.go
+++ version/version.go
@@ -1,11 +1,33 @@
 // Package version defines aws-k8s-tester version.
 package version
 
+import (
+	"fmt"
+	"time"
+)
+
 var (
 	// GitCommit is the git commit on build.
 	GitCommit = ""
 	// ReleaseVersion is the release version.
-	ReleaseVersion = "TODO"
+	ReleaseVersion = ""
 	// BuildTime is the build timestamp.
 	BuildTime = ""
 )
+
+func init() {
+	now := time.Now()
+	if ReleaseVersion == "" {
+		ReleaseVersion = fmt.Sprintf(
+			"%d%02d%02d%02d%02d",
+			now.Year(),
+			int(now.Month()),
+			now.Day(),
+			now.Hour(),
+			now.Minute(),
+		)
+	}
+	if BuildTime == "" {
+		BuildTime = now.String()
+	}
+}
