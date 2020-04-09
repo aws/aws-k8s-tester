3dac533adcf2fb0aa51f19d4f56bbc9dd2b59eb5
Gyuho Lee <leegyuho@amazon.com>
2020-04-09 13:53:50 -0700

    pkg/k8s-client: add health checks
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- pkg/k8s-client/eks-client.go
+++ pkg/k8s-client/eks-client.go
@@ -1,9 +1,18 @@
 package k8sclient
 
 import (
+	"bufio"
+	"bytes"
+	"context"
+	"crypto/tls"
 	"encoding/base64"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,15 +20,19 @@ import (
 	"github.com/aws/aws-sdk-go/service/sts"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	clientset "k8s.io/client-go/kubernetes"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"k8s.io/utils/exec"
 )
 
 // EKSConfig defines EKS client configuration.
 type EKSConfig struct {
+	Logger *zap.Logger
+
 	Region string
 
 	ClusterName              string
@@ -51,42 +64,168 @@ type EKSConfig struct {
 	// ref. https://github.com/kubernetes/kubernetes/blob/4d0e86f0b8d1eae00a202009858c8739e4c9402e/staging/src/k8s.io/apiserver/pkg/server/config.go#L300-L301
 	ClientBurst int
 
-	KubeConfigPath string
+	KubectlPath string
+
+	KubeConfigPath    string
+	KubeConfigContext string
+
+	ServerVersion string
+
+	EncryptionEnabled bool
+}
+
+// EKS defines EKS client operations.
+type EKS interface {
+	// KubernetesClientSet returns a new kubernetes client set.
+	KubernetesClientSet() *kubernetes.Clientset
+	// CheckEKSHealth checks the EKS health.
+	CheckHealth() error
+}
+
+type eks struct {
+	cfg *EKSConfig
+	cli *kubernetes.Clientset
+	mu  sync.Mutex
+}
+
+// KubernetesClientSet returns a new kubernetes client set.
+func (e *eks) KubernetesClientSet() *kubernetes.Clientset {
+	return e.cli
+}
+
+// CheckHealth checks the EKS health.
+func (e *eks) CheckHealth() error {
+	// allow only one health check at a time
+	e.mu.Lock()
+	err := e.checkHealth()
+	e.mu.Unlock()
+	return err
 }
 
 // NewEKS returns a new EKS client.
-func NewEKS(lg *zap.Logger, cfg EKSConfig) (cli *kubernetes.Clientset, err error) {
-	if lg == nil {
-		lg = zap.NewExample()
+func NewEKS(cfg *EKSConfig) (EKS, error) {
+	if cfg == nil {
+		return nil, errors.New("nil EKSConfig")
 	}
-	kc := createClientConfigEKS(lg, cfg)
-	if kc == nil {
-		lg.Info("creating k8s client using KUBECONFIG")
-		kc, err = clientcmd.BuildConfigFromFlags("", cfg.KubeConfigPath)
+	if cfg.Logger == nil {
+		cfg.Logger = zap.NewExample()
+	}
+	var kcfg *restclient.Config
+	var err error
+	if cfg.KubeConfigPath != "" {
+		switch {
+		case cfg.KubeConfigContext != "":
+			cfg.Logger.Info("creating k8s client using KUBECONFIG and context",
+				zap.String("kubeconfig", cfg.KubeConfigPath),
+				zap.String("context", cfg.KubeConfigContext),
+			)
+			kcfg, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+				&clientcmd.ClientConfigLoadingRules{
+					ExplicitPath: cfg.KubeConfigPath,
+				},
+				&clientcmd.ConfigOverrides{
+					CurrentContext: cfg.KubeConfigContext,
+					ClusterInfo:    clientcmdapi.Cluster{Server: ""},
+				},
+			).ClientConfig()
+		case cfg.KubeConfigContext == "":
+			cfg.Logger.Info("creating k8s client using KUBECONFIG",
+				zap.String("kubeconfig", cfg.KubeConfigPath),
+			)
+			kcfg, err = clientcmd.BuildConfigFromFlags("", cfg.KubeConfigPath)
+		}
 		if err != nil {
-			lg.Warn("failed to read kubeconfig", zap.Error(err))
-			return nil, err
+			cfg.Logger.Warn("failed to read kubeconfig", zap.Error(err))
 		}
 	}
-	if cfg.ClientQPS > 0 {
-		kc.QPS = cfg.ClientQPS
+	if kcfg == nil {
+		kcfg = createClientConfigEKS(cfg)
 	}
-	if cfg.ClientBurst > 0 {
-		kc.Burst = cfg.ClientBurst
+	if kcfg == nil {
+		cfg.Logger.Warn("failed to create k8s client config")
+		return nil, errors.New("failed to create k8s client config")
 	}
 
-	cli, err = clientset.NewForConfig(kc)
+	cfg.Logger.Info("loaded k8s client config",
+		zap.String("host", kcfg.Host),
+		zap.String("user-name", kcfg.Username),
+		zap.String("server-name", kcfg.ServerName),
+	)
+
+	if cfg.ClusterAPIServerEndpoint == "" {
+		cfg.ClusterAPIServerEndpoint = kcfg.Host
+		cfg.Logger.Info("updated apiserver endpoint from kubeconfig", zap.String("apiserver-endpoint", kcfg.Host))
+	} else if cfg.ClusterAPIServerEndpoint != kcfg.Host {
+		cfg.Logger.Warn("unexpected apiserver endpoint",
+			zap.String("apiserver-endpoint", cfg.ClusterAPIServerEndpoint),
+			zap.String("kubeconfig-host", kcfg.Host),
+		)
+	}
+	if cfg.ClusterAPIServerEndpoint == "" {
+		return nil, errors.New("empty ClusterAPIServerEndpoint")
+	}
+
+	if cfg.ClusterCADecoded == "" {
+		cfg.ClusterCADecoded = string(kcfg.TLSClientConfig.CAData)
+		cfg.Logger.Info("updated cluster ca from kubeconfig", zap.String("cluster-ca", string(kcfg.TLSClientConfig.CAData)))
+	} else if cfg.ClusterCADecoded != string(kcfg.TLSClientConfig.CAData) {
+		cfg.Logger.Warn("unexpected cluster ca",
+			zap.String("cluster-ca", cfg.ClusterCADecoded),
+			zap.String("kubeconfig-cluster-ca", string(kcfg.TLSClientConfig.CAData)),
+		)
+	}
+	if cfg.ClusterCADecoded == "" {
+		return nil, errors.New("empty ClusterCADecoded")
+	}
+
+	if kcfg.AuthProvider != nil {
+		if cfg.Region == "" {
+			cfg.Region = kcfg.AuthProvider.Config["region"]
+			cfg.Logger.Info("updated region from kubeconfig", zap.String("apiserver-endpoint", kcfg.AuthProvider.Config["region"]))
+		} else if cfg.Region != kcfg.AuthProvider.Config["region"] {
+			cfg.Logger.Warn("unexpected region",
+				zap.String("apiserver-endpoint", cfg.Region),
+				zap.String("kubeconfig-host", kcfg.AuthProvider.Config["region"]),
+			)
+		}
+		if cfg.ClusterName == "" {
+			cfg.ClusterName = kcfg.AuthProvider.Config["cluster-name"]
+			cfg.Logger.Info("updated cluster-name from kubeconfig", zap.String("apiserver-endpoint", kcfg.AuthProvider.Config["cluster-name"]))
+		} else if cfg.ClusterName != kcfg.AuthProvider.Config["cluster-name"] {
+			cfg.Logger.Warn("unexpected cluster-name",
+				zap.String("apiserver-endpoint", cfg.ClusterName),
+				zap.String("kubeconfig-host", kcfg.AuthProvider.Config["cluster-name"]),
+			)
+		}
+	}
+	if cfg.Region == "" {
+		cfg.Logger.Warn("no region found in k8s client")
+	}
+	if cfg.ClusterName == "" {
+		cfg.Logger.Warn("no cluster name found in k8s client")
+	}
+
+	if cfg.ClientQPS > 0 {
+		kcfg.QPS = cfg.ClientQPS
+	}
+	if cfg.ClientBurst > 0 {
+		kcfg.Burst = cfg.ClientBurst
+	}
+
+	ek := &eks{cfg: cfg}
+	ek.cli, err = clientset.NewForConfig(kcfg)
 	if err != nil {
-		lg.Warn("failed to create k8s client", zap.Error(err))
+		cfg.Logger.Warn("failed to create k8s client", zap.Error(err))
 		return nil, err
 	}
-	lg.Info("created k8s client", zap.Float32("qps", kc.QPS), zap.Int("burst", kc.Burst))
-	return cli, nil
+	cfg.Logger.Info("created k8s client", zap.Float32("qps", kcfg.QPS), zap.Int("burst", kcfg.Burst))
+
+	return ek, nil
 }
 
 const authProviderName = "eks"
 
-func createClientConfigEKS(lg *zap.Logger, cfg EKSConfig) *restclient.Config {
+func createClientConfigEKS(cfg *EKSConfig) *restclient.Config {
 	if cfg.Region == "" {
 		return nil
 	}
@@ -99,7 +238,7 @@ func createClientConfigEKS(lg *zap.Logger, cfg EKSConfig) *restclient.Config {
 	if cfg.ClusterCADecoded == "" {
 		return nil
 	}
-	lg.Info("creating k8s client using status")
+	cfg.Logger.Info("creating k8s client using status")
 	return &restclient.Config{
 		Host: cfg.ClusterAPIServerEndpoint,
 		TLSClientConfig: restclient.TLSClientConfig{
@@ -181,4 +320,236 @@ func (s *eksTokenSource) Token() (*oauth2.Token, error) {
 		TokenType:   "Bearer",
 		Expiry:      tokenExpiration,
 	}, nil
+}
+
+func (e *eks) checkHealth() error {
+	if e.cfg == nil {
+		return errors.New("nil EKSConfig")
+	}
+	if e.cfg.KubectlPath == "" {
+		return errors.New("empty EKSConfig.KubectlPath")
+	}
+	if e.cfg.KubeConfigPath == "" {
+		return errors.New("empty EKSConfig.KubeConfigPath")
+	}
+	if e.cfg.ClusterAPIServerEndpoint == "" {
+		return errors.New("empty EKSConfig.ClusterAPIServerEndpoint")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	output, err := exec.New().CommandContext(
+		ctx,
+		e.cfg.KubectlPath,
+		"--kubeconfig="+e.cfg.KubeConfigPath,
+		"version",
+	).CombinedOutput()
+	cancel()
+	out := string(output)
+	if err != nil {
+		return fmt.Errorf("'kubectl version' failed %v (output %q)", err, out)
+	}
+	fmt.Printf("\n\"kubectl version\" output:\n%s\n", out)
+
+	ep := e.cfg.ClusterAPIServerEndpoint + "/version"
+	buf := bytes.NewBuffer(nil)
+	if err = httpReadInsecure(e.cfg.Logger, ep, buf); err != nil {
+		return err
+	}
+	out = buf.String()
+	if e.cfg.ServerVersion != "" && !strings.Contains(out, fmt.Sprintf(`"gitVersion": "v%s`, e.cfg.ServerVersion)) {
+		return fmt.Errorf("%q does not contain version %q", out, e.cfg.ServerVersion)
+	}
+	fmt.Printf("\n\n\"%s\" output:\n%s\n", ep, out)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
+	output, err = exec.New().CommandContext(
+		ctx,
+		e.cfg.KubectlPath,
+		"--kubeconfig="+e.cfg.KubeConfigPath,
+		"cluster-info",
+	).CombinedOutput()
+	cancel()
+	out = string(output)
+	if err != nil {
+		return fmt.Errorf("'kubectl cluster-info' failed %v (output %q)", err, out)
+	}
+	if !strings.Contains(out, "is running at") {
+		return fmt.Errorf("'kubectl cluster-info' not ready (output %q)", out)
+	}
+	fmt.Printf("\n\"kubectl cluster-info\" output:\n%s\n", out)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
+	output, err = exec.New().CommandContext(
+		ctx,
+		e.cfg.KubectlPath,
+		"--kubeconfig="+e.cfg.KubeConfigPath,
+		"get",
+		"cs",
+	).CombinedOutput()
+	cancel()
+	out = string(output)
+	if err != nil {
+		return fmt.Errorf("'kubectl get cs' failed %v (output %q)", err, out)
+	}
+	fmt.Printf("\n\"kubectl get cs\" output:\n%s\n", out)
+
+	ep = e.cfg.ClusterAPIServerEndpoint + "/healthz?verbose"
+	buf.Reset()
+	if err := httpReadInsecure(e.cfg.Logger, ep, buf); err != nil {
+		return err
+	}
+	out = buf.String()
+	if !strings.Contains(out, "healthz check passed") {
+		return fmt.Errorf("%q does not contain 'healthz check passed'", out)
+	}
+	fmt.Printf("\n\n\"%s\" output (\"kubectl get --raw /healthz?verbose\"):\n%s\n", ep, out)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
+	output, err = exec.New().CommandContext(
+		ctx,
+		e.cfg.KubectlPath,
+		"--kubeconfig="+e.cfg.KubeConfigPath,
+		"--namespace=kube-system",
+		"get",
+		"all",
+	).CombinedOutput()
+	cancel()
+	out = string(output)
+	if err != nil {
+		return fmt.Errorf("'kubectl get all -n=kube-system' failed %v (output %q)", err, out)
+	}
+	fmt.Printf("\n\"kubectl all -n=kube-system\" output:\n%s", out)
+
+	fmt.Printf("\n\"kubectl get pods -n=kube-system\" output:\n")
+	pods, err := e.cli.CoreV1().Pods("kube-system").List(metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to get pods %v", err)
+	}
+	for _, v := range pods.Items {
+		fmt.Printf("kube-system Pod: %q\n", v.Name)
+	}
+	println()
+
+	ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
+	output, err = exec.New().CommandContext(
+		ctx,
+		e.cfg.KubectlPath,
+		"--kubeconfig="+e.cfg.KubeConfigPath,
+		"get",
+		"configmaps",
+		"--namespace=kube-system",
+	).CombinedOutput()
+	cancel()
+	out = string(output)
+	if err != nil {
+		return fmt.Errorf("'kubectl get configmaps --namespace=kube-system' failed %v (output %q)", err, out)
+	}
+	fmt.Printf("\n\"kubectl get configmaps --namespace=kube-system\" output:\n%s\n", out)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 15*time.Second)
+	output, err = exec.New().CommandContext(
+		ctx,
+		e.cfg.KubectlPath,
+		"--kubeconfig="+e.cfg.KubeConfigPath,
+		"get",
+		"namespaces",
+	).CombinedOutput()
+	cancel()
+	out = string(output)
+	if err != nil {
+		return fmt.Errorf("'kubectl get namespaces' failed %v (output %q)", err, out)
+	}
+	fmt.Printf("\n\"kubectl get namespaces\" output:\n%s\n", out)
+
+	fmt.Printf("\n\"curl -sL http://localhost:8080/metrics | grep storage_\" output:\n")
+	output, err = e.cli.
+		CoreV1().
+		RESTClient().
+		Get().
+		RequestURI("/metrics").
+		Do().
+		Raw()
+	if err != nil {
+		return fmt.Errorf("failed to fetch /metrics (%v)", err)
+	}
+	const (
+		metricDEKGen            = "apiserver_storage_data_key_generation_latencies_microseconds_count"
+		metricEnvelopeCacheMiss = "apiserver_storage_envelope_transformation_cache_misses_total"
+	)
+	dekGenCnt, cacheMissCnt := int64(0), int64(0)
+	scanner := bufio.NewScanner(bytes.NewReader(output))
+	for scanner.Scan() {
+		line := scanner.Text()
+		switch {
+		case strings.HasPrefix(line, "# "):
+			continue
+
+		case strings.HasPrefix(line, metricDEKGen+" "):
+			vs := strings.TrimSpace(strings.Replace(line, metricDEKGen, "", -1))
+			dekGenCnt, err = strconv.ParseInt(vs, 10, 64)
+			if err != nil {
+				e.cfg.Logger.Warn("failed to parse",
+					zap.String("line", line),
+					zap.Error(err),
+				)
+			}
+
+		case strings.HasPrefix(line, metricEnvelopeCacheMiss+" "):
+			vs := strings.TrimSpace(strings.Replace(line, metricEnvelopeCacheMiss, "", -1))
+			cacheMissCnt, err = strconv.ParseInt(vs, 10, 64)
+			if err != nil {
+				e.cfg.Logger.Warn("failed to parse",
+					zap.String("line", line),
+					zap.Error(err),
+				)
+			}
+		}
+
+		if dekGenCnt > 0 || cacheMissCnt > 0 {
+			break
+		}
+	}
+	e.cfg.Logger.Info("encryption metrics",
+		zap.Int64("dek-gen-count", dekGenCnt),
+		zap.Int64("cache-miss-count", cacheMissCnt),
+	)
+	if e.cfg.EncryptionEnabled {
+		if dekGenCnt <= 0 && cacheMissCnt <= 0 {
+			return errors.New("encrypted enabled, unexpected /metrics")
+		}
+		e.cfg.Logger.Info("successfully checked encryption")
+	}
+
+	e.cfg.Logger.Info("checked /metrics")
+	return nil
+}
+
+// curl -k [URL]
+func httpReadInsecure(lg *zap.Logger, u string, wr io.Writer) error {
+	lg.Info("reading", zap.String("url", u))
+	cli := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}}
+	r, err := cli.Get(u)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	if r.StatusCode >= 400 {
+		return fmt.Errorf("%q returned %d", u, r.StatusCode)
+	}
+
+	_, err = io.Copy(wr, r.Body)
+	if err != nil {
+		lg.Warn("failed to read", zap.String("url", u), zap.Error(err))
+	} else {
+		lg.Info("read",
+			zap.String("url", u),
+		)
+	}
+	return err
 }
