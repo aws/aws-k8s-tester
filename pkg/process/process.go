5d0072fbd7bd5376e92cfdeb07d29ef70e1da1bf
Gyuho Lee <leegyuho@amazon.com>
2020-04-15 14:35:32 -0700

    pkg/process: godoc
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- pkg/process/process.go
+++ pkg/process/process.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package process implements process utilities.
 package process
 
 import (
