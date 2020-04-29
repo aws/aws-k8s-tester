89e7039ab99ea1377fc88fa0de38190533c21d74
Gyuho Lee <leegyuho@amazon.com>
2020-04-29 01:16:37 -0700

    eks/helm: improve install error message
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- eks/helm/helm.go
+++ eks/helm/helm.go
@@ -299,7 +299,7 @@ func Install(cfg InstallConfig) (err error) {
 		}
 	}
 
-	return err
+	return fmt.Errorf("failed to install chart %q (version %q) with error %v", chart.Name(), chart.AppVersion(), err)
 }
 
 // Uninstall uninstalls a helm chart.
