840a4ff668bde87b72f0be3696817bcd99a86b4e
Gyuho Lee <leegyuho@amazon.com>
2020-04-30 10:49:06 -0700

    scripts: add gen scripts
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

---
+++ scripts/ec2config.gen.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+if ! [[ "$0" =~ scripts/ec2config.gen.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+rm -f ec2config/README.md
+go run ec2config/gen/main.go
+cat ec2config/README.md
