95ea8aac9400b9e547ae08eadca643565ff0b3b7
Gyuho Lee <leegyuho@amazon.com>
2020-07-14 03:06:01 -0700

    pkg/user: check length
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -9,22 +9,36 @@ import (
 )
 
 // Get returns the current user name and ID.
-func Get() string {
+func Get() (uv string) {
 	u, err := user.Current()
 	if err != nil {
 		return fmt.Sprintf("user=%s", os.Getenv("USER"))
 	}
+
+	homeDir := u.HomeDir
+	if len(homeDir) > 65 {
+		homeDir = "..." + homeDir[len(homeDir)-65:]
+	}
+
 	h, err := os.Hostname()
 	if err != nil {
 		h = os.Getenv("HOSTNAME")
 	}
+	if len(h) > 65 {
+		h = "..." + h[len(h)-65:]
+	}
+
 	// ref. https://docs.aws.amazon.com/general/latest/gr/aws_tagging.html#tag-conventions
-	return fmt.Sprintf("name=%s_user=%s_home=%s_hostname=%s_os=%s_arch=%s",
+	uv = fmt.Sprintf("name=%s_user=%s_home=%s_hostname=%s_os=%s_arch=%s",
 		u.Name,
 		u.Username,
-		u.HomeDir,
+		homeDir,
 		h,
 		runtime.GOOS,
 		runtime.GOARCH,
 	)
+	if len(uv) > 253 {
+		uv = uv[:250] + "..."
+	}
+	return uv
 }
