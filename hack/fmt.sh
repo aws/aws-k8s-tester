5cedffeb0aeb98e5e739ee631bb8e9c2a3807516
Ellis Tarn <etarn@amazon.com>
2020-07-22 13:53:04 -0700

    Renamed scripts to hack for parity with k/k style: https://github.com/kubernetes/kubernetes/tree/master/hack

--- hack/fmt.sh
+++ hack/fmt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if ! [[ "$0" =~ scripts/fmt.sh ]]; then
+if ! [[ "$0" =~ hack/fmt.sh ]]; then
   echo "must be run from repository root"
   exit 255
 fi
--- hack/unit.sh
+++ hack/unit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if ! [[ "$0" =~ scripts/unit.sh ]]; then
+if ! [[ "$0" =~ hack/unit.sh ]]; then
   echo "must be run from repository root"
   exit 255
 fi
