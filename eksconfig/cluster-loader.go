20b1b6eb733e08a73db1d3f196690239afcfcfc0
njtran <nichotr@amazon.com>
2020-09-04 14:47:19 -0700

    Added defaults for CL2 add-on

--- eksconfig/cluster-loader.go
+++ eksconfig/cluster-loader.go
@@ -1,5 +1,7 @@
 package eksconfig
 
+import "strings"
+
 // ClusterLoaderSpec defines the spec for the Addon
 type ClusterLoaderSpec struct {
 	Image          string   `json:"image,omitempty"`
@@ -21,4 +23,37 @@ func (spec *ClusterLoaderSpec) Validate(cfg *Config) error {
 
 // Default installs the addon
 func (spec *ClusterLoaderSpec) Default(cfg *Config) {
+	if spec.Image == "" {
+		spec.Image = "197575167141.dkr.ecr.us-west-2.amazonaws.com/clusterloader2:latest"
+	}
+	if len(spec.TestConfigUris) == 0 {
+		spec.TestConfigUris = []string{
+			"https://raw.githubusercontent.com/aws/aws-k8s-tester/master/eks/cluster-loader/configs/cluster-autoscaler/config.yaml",
+			"https://raw.githubusercontent.com/aws/aws-k8s-tester/master/eks/cluster-loader/configs/cluster-autoscaler/deployment.yaml",
+		}
+	}
+	set := make(map[string]bool)
+	for _, param := range spec.TestParams {
+		tempSplit := strings.Split(param, "=")
+		if len(tempSplit) > 1 {
+			set[tempSplit[0]] = true
+		}
+		set[param] = true
+	}
+	if ok := set["--run-from-cluster"]; !ok {
+		spec.TestParams = append(spec.TestParams, "--run-from-cluster")
+	}
+	if ok := set["--testconfig"]; !ok {
+		spec.TestParams = append(spec.TestParams, "--testconfig=/etc/config/config.yaml")
+	}
+	if ok := set["--provider"]; !ok {
+		spec.TestParams = append(spec.TestParams, "--provider=eks")
+	}
+	if ok := set["--nodes"]; !ok {
+		spec.TestParams = append(spec.TestParams, "--nodes=10")
+	}
+	if ok := set["--alsologtostderr"]; !ok {
+		spec.TestParams = append(spec.TestParams, "--alsologtostderr")
+	}
+
 }
