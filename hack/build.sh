38e1934ad77be9f04ef9f6f1c485bee8ead0a583
Gyuho Lee <leegyuho@amazon.com>
2021-01-25 14:13:10 -0800

    hack/build.sh: add "arm64" builds
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- hack/build.sh
+++ hack/build.sh
@@ -13,32 +13,40 @@ echo "GIT_COMMIT:" ${GIT_COMMIT}
 echo "RELEASE_VERSION:" ${RELEASE_VERSION}
 echo "BUILD_TIME:" ${BUILD_TIME}
 
-DEFAULT_WHAT='aws-k8s-tester cw-utils ec2-utils ecr-utils eks-utils etcd-utils s3-utils sts-utils'
+DEFAULT_ARCHS='amd64 arm64'
 DEFAULT_TARGETS='linux darwin'
+DEFAULT_WHAT='aws-k8s-tester cw-utils ec2-utils ecr-utils eks-utils etcd-utils s3-utils sts-utils'
 
-PACKAGE_NAME='github.com/aws/aws-k8s-tester'
-WHAT=${WHAT:-$DEFAULT_WHAT}
+ARCHS=${ARCHS:-$DEFAULT_ARCHS}
 TARGETS=${TARGETS:-$DEFAULT_TARGETS}
+WHAT=${WHAT:-$DEFAULT_WHAT}
 
 echo ""
 echo "Usage: \`make TARGETS='linux' WHAT='aws-k8s-tester cw-utils'\`"
+echo "DEFAULT_ARCHS=$DEFAULT_ARCHS"
 echo "DEFAULT_TARGETS=$DEFAULT_TARGETS"
 echo "DEFAULT_WHAT=$DEFAULT_WHAT"
 echo ""
 
 mkdir -p ./bin
 
-for os in ${TARGETS}; do
-  for cmd in ${WHAT}; do
-    echo "=== Building target=${cmd}, os=${os} ==="
-    CGO_ENABLED=0 GOOS=${os} GOARCH=$(go env GOARCH) \
-      go build -mod=vendor -v \
-      -ldflags "-s -w \
-      -X ${PACKAGE_NAME}/version.GitCommit=${GIT_COMMIT} \
-      -X ${PACKAGE_NAME}/version.ReleaseVersion=${RELEASE_VERSION} \
-      -X ${PACKAGE_NAME}/version.BuildTime=${BUILD_TIME}" \
-      -o ./bin/${cmd}-${RELEASE_VERSION}-${os}-$(go env GOARCH) \
-      ./cmd/${cmd}
+PACKAGE_NAME='github.com/aws/aws-k8s-tester'
+for arch in ${ARCHS}; do
+  for os in ${TARGETS}; do
+    for bin in ${WHAT}; do
+      echo "=== Building arch=${arch}, os=${os}, target=${bin} ==="
+      CGO_ENABLED=0 \
+        GOARCH=${arch} \
+        GOOS=${os} \
+        go build \
+        -mod=vendor -v \
+        -ldflags "-s -w \
+        -X ${PACKAGE_NAME}/version.GitCommit=${GIT_COMMIT} \
+        -X ${PACKAGE_NAME}/version.ReleaseVersion=${RELEASE_VERSION} \
+        -X ${PACKAGE_NAME}/version.BuildTime=${BUILD_TIME}" \
+        -o ./bin/${bin}-${RELEASE_VERSION}-${os}-$(go env GOARCH) \
+        ./cmd/${bin}
+    done
   done
 done
 
