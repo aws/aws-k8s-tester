ebe96e838950abc14f1016532e715112d5624f01
Gyuho Lee <leegyuho@amazon.com>
2021-05-28 10:50:14 -0700

    kubernetes-dashboard: initial commit
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

---
+++ k8s-tester/kubernetes-dashboard/cmd/k8s-tester-kubernetes-dashboard/main.go
@@ -0,0 +1,133 @@
+// k8s-tester-kubernetes-dashboard installs a Kubernetes dashboard.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-k8s-tester/client"
+	kubernetes_dashboard "github.com/aws/aws-k8s-tester/k8s-tester/kubernetes-dashboard"
+	"github.com/aws/aws-k8s-tester/utils/log"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+var rootCmd = &cobra.Command{
+	Use:        "k8s-tester-kubernetes-dashboard",
+	Short:      "Kubernetes kubernetes-dashboard tester",
+	SuggestFor: []string{"kubernetes-dashboard"},
+}
+
+func init() {
+	cobra.EnablePrefixMatching = true
+}
+
+var (
+	enablePrompt   bool
+	logLevel       string
+	logOutputs     []string
+	minimumNodes   int
+	namespace      string
+	kubectlPath    string
+	kubeConfigPath string
+)
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&enablePrompt, "enable-prompt", true, "'true' to enable prompt mode")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", log.DefaultLogLevel, "Logging level")
+	rootCmd.PersistentFlags().StringSliceVar(&logOutputs, "log-outputs", []string{"stderr"}, "Additional logger outputs")
+	rootCmd.PersistentFlags().IntVar(&minimumNodes, "minimum-nodes", kubernetes_dashboard.DefaultMinimumNodes, "minimum number of Kubernetes nodes required for installing this addon")
+	rootCmd.PersistentFlags().StringVar(&namespace, "namespace", "test-namespace", "'true' to auto-generate path for create config/cluster, overwrites existing --path value")
+	rootCmd.PersistentFlags().StringVar(&kubectlPath, "kubectl-path", "", "kubectl path")
+	rootCmd.PersistentFlags().StringVar(&kubeConfigPath, "kubeconfig-path", "", "KUBECONFIG path")
+
+	rootCmd.AddCommand(
+		newApply(),
+		newDelete(),
+	)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "k8s-tester-kubernetes-dashboard failed %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func newApply() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "apply",
+		Short: "Apply tests",
+		Run:   createApplyFunc,
+	}
+	return cmd
+}
+
+func createApplyFunc(cmd *cobra.Command, args []string) {
+	lg, logWriter, _, err := log.NewWithStderrWriter(logLevel, logOutputs)
+	if err != nil {
+		panic(err)
+	}
+	_ = zap.ReplaceGlobals(lg)
+
+	cfg := kubernetes_dashboard.Config{
+		EnablePrompt: enablePrompt,
+		Logger:       lg,
+		LogWriter:    logWriter,
+		MinimumNodes: minimumNodes,
+		Namespace:    namespace,
+		ClientConfig: &client.Config{
+			Logger:         lg,
+			KubectlPath:    kubectlPath,
+			KubeConfigPath: kubeConfigPath,
+		},
+	}
+
+	ts := kubernetes_dashboard.New(cfg)
+	if err := ts.Apply(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to apply (%v)\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("\n*********************************\n")
+	fmt.Printf("'k8s-tester-kubernetes-dashboard apply' success\n")
+}
+
+func newDelete() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Short: "Delete resources",
+		Run:   createDeleteFunc,
+	}
+	return cmd
+}
+
+func createDeleteFunc(cmd *cobra.Command, args []string) {
+	lg, logWriter, _, err := log.NewWithStderrWriter(logLevel, logOutputs)
+	if err != nil {
+		panic(err)
+	}
+	_ = zap.ReplaceGlobals(lg)
+
+	cfg := kubernetes_dashboard.Config{
+		EnablePrompt: enablePrompt,
+		Logger:       lg,
+		LogWriter:    logWriter,
+		Namespace:    namespace,
+		ClientConfig: &client.Config{
+			Logger:         lg,
+			KubectlPath:    kubectlPath,
+			KubeConfigPath: kubeConfigPath,
+		},
+	}
+
+	ts := kubernetes_dashboard.New(cfg)
+	if err := ts.Delete(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to delete (%v)\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("\n*********************************\n")
+	fmt.Printf("'k8s-tester-kubernetes-dashboard delete' success\n")
+}
