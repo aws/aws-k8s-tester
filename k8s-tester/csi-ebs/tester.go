899507ec84c427a363a14e73fd54a3580d1ac785
Gyuho Lee <leegyuho@amazon.com>
2021-09-26 12:31:37 -0700

    k8s-tester: clean up "csi-ebs"
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- k8s-tester/csi-ebs/tester.go
+++ k8s-tester/csi-ebs/tester.go
@@ -25,6 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	api_resource "k8s.io/apimachinery/pkg/api/resource"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/utils/exec"
 )
 
@@ -567,5 +568,73 @@ func (ts *tester) deletevolumePods() error {
 		return fmt.Errorf("failed to delete Pod (%v)", err)
 	}
 	ts.cfg.Logger.Info("deleted a Pod for EBS Tests")
+	return nil
+}
+
+// getBoundPV returns a PV details.
+func (ts *tester) getBoundPV(pvc *v1.PersistentVolumeClaim) (*v1.PersistentVolume, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	// Get new copy of the claim
+	claim, err := ts.cfg.Client.KubernetesClient().CoreV1().PersistentVolumeClaims(pvc.Namespace).Get(ctx, pvc.Name, meta_v1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	// Get the bound PV
+	pv, err := ts.cfg.Client.KubernetesClient().CoreV1().PersistentVolumes().Get(ctx, claim.Spec.VolumeName, meta_v1.GetOptions{})
+	cancel()
+	return pv, err
+}
+
+// expandPVCSize expands PVC size
+func (ts *tester) expandPVCSize(origPVC *v1.PersistentVolumeClaim, size resource.Quantity) (*v1.PersistentVolumeClaim, error) {
+	pvcName := origPVC.Name
+	updatedPVC := origPVC.DeepCopy()
+	var resizePollInterval = 2 * time.Second
+	// Retry the update on error, until we hit a timeout.
+	// TODO: Determine whether "retry with timeout" is appropriate here. Maybe we should only retry on version conflict.
+	var lastUpdateError error
+	waitErr := wait.PollImmediate(resizePollInterval, 30*time.Second, func() (bool, error) {
+		var err error
+		updatedPVC, err = ts.cfg.Client.KubernetesClient().CoreV1().PersistentVolumeClaims(origPVC.Namespace).Get(context.TODO(), pvcName, meta_v1.GetOptions{})
+		if err != nil {
+			return false, fmt.Errorf("error fetching pvc %q for resizing: %v", pvcName, err)
+		}
+		updatedPVC.Spec.Resources.Requests[v1.ResourceStorage] = size
+		updatedPVC, err = ts.cfg.Client.KubernetesClient().CoreV1().PersistentVolumeClaims(origPVC.Namespace).Update(context.TODO(), updatedPVC, meta_v1.UpdateOptions{})
+		if err != nil {
+			return false, fmt.Errorf("error fetching pvc %q for resizing: %v", updatedPVC, err)
+		}
+		return true, nil
+	})
+	if waitErr == wait.ErrWaitTimeout {
+		return nil, fmt.Errorf("timed out attempting to update PVC size. last update error: %v", lastUpdateError)
+	}
+	if waitErr != nil {
+		return nil, fmt.Errorf("failed to expand PVC size (check logs for error): %v", waitErr)
+	}
+	return updatedPVC, nil
+}
+
+func (ts *tester) waitForControllerVolumeResize(pvc *v1.PersistentVolumeClaim, timeout time.Duration) error {
+	pvName := pvc.Spec.VolumeName
+	var resizePollInterval = 2 * time.Second
+	waitErr := wait.PollImmediate(resizePollInterval, timeout, func() (bool, error) {
+		pvcSize := pvc.Spec.Resources.Requests[v1.ResourceStorage]
+		pv, err := ts.cfg.Client.KubernetesClient().CoreV1().PersistentVolumes().Get(context.TODO(), pvName, meta_v1.GetOptions{})
+		if err != nil {
+			return false, fmt.Errorf("error fetching pv %q for resizing %v", pvName, err)
+		}
+
+		pvSize := pv.Spec.Capacity[v1.ResourceStorage]
+
+		// If pv size is greater or equal to requested size that means controller resize is finished.
+		if pvSize.Cmp(pvcSize) >= 0 {
+			return true, nil
+		}
+		return false, nil
+	})
+	if waitErr != nil {
+		return fmt.Errorf("error while waiting for controller resize to finish: %v", waitErr)
+	}
 	return nil
 }
