fc54f0d2a4855c8e7153619b314ef0b36b9ae8a9
Jay Deokar <23660509+jaydeokar@users.noreply.github.com>
2024-01-12 00:30:02 -0800

    Adding Network Policy test (#395)

--- e2e2/go.mod
+++ e2e2/go.mod
@@ -3,9 +3,11 @@ module github.com/aws/aws-k8s-tester/e2e2
 go 1.21
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.24.1
+	github.com/aws/aws-sdk-go-v2/config v1.26.3
+	github.com/aws/aws-sdk-go-v2/service/eks v1.37.1
 	github.com/kubeflow/mpi-operator v0.4.0
-	github.com/stretchr/testify v1.8.4
-	gopkg.in/yaml.v2 v2.4.0
+	github.com/pkg/errors v0.9.1
 	k8s.io/api v0.28.3
 	k8s.io/apimachinery v0.28.3
 	k8s.io/cli-runtime v0.28.3
@@ -14,6 +16,17 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.14 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.7 // indirect
+	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.10.2 // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
@@ -31,6 +44,7 @@ require (
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/imdario/mergo v0.3.15 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kubeflow/common v0.4.6 // indirect
@@ -40,14 +54,11 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/xlab/treeprint v1.2.0 // indirect
 	go.starlark.net v0.0.0-20230525235612-a134d8f9ddca // indirect
-	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
@@ -58,7 +69,9 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apiextensions-apiserver v0.28.2 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
---
+++ e2e2/test/cases/netpol/np_test.go
@@ -0,0 +1,209 @@
+package netpol
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"strings"
+	"testing"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	networking "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+func TestNetworkPolicyCases(t *testing.T) {
+
+	protocolTCP := corev1.ProtocolTCP
+	protocolUDP := corev1.ProtocolUDP
+	networkPolicy := networking.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{Name: "block-c-to-a", Namespace: "a"},
+		Spec: networking.NetworkPolicySpec{
+			PodSelector: metav1.LabelSelector{MatchLabels: map[string]string{"app": "a-server"}},
+			PolicyTypes: []networking.PolicyType{networking.PolicyTypeIngress, networking.PolicyTypeEgress},
+			Ingress: []networking.NetworkPolicyIngressRule{
+				{
+					From: []networking.NetworkPolicyPeer{
+						{
+							PodSelector:       &metav1.LabelSelector{MatchLabels: map[string]string{"app": "b-server"}},
+							NamespaceSelector: &metav1.LabelSelector{MatchLabels: map[string]string{"ns": "b"}},
+						},
+					},
+					Ports: []networking.NetworkPolicyPort{
+						{
+							Protocol: &protocolTCP,
+							Port:     &intstr.IntOrString{IntVal: 80},
+						},
+					},
+				},
+			},
+			Egress: []networking.NetworkPolicyEgressRule{
+				{
+					To: []networking.NetworkPolicyPeer{
+						{
+							PodSelector:       &metav1.LabelSelector{MatchLabels: map[string]string{"app": "b-server"}},
+							NamespaceSelector: &metav1.LabelSelector{MatchLabels: map[string]string{"ns": "b"}},
+						},
+					},
+					Ports: []networking.NetworkPolicyPort{
+						{
+							Protocol: &protocolTCP,
+							Port:     &intstr.IntOrString{IntVal: 80},
+						},
+					},
+				},
+				{
+					Ports: []networking.NetworkPolicyPort{
+						{
+							Protocol: &protocolUDP,
+							Port:     &intstr.IntOrString{IntVal: 53},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	allowAll := features.New("allowAll").
+		WithLabel("suite", "netpol").
+		WithLabel("policy", "none").
+		Assess("curl from A to B succeeds", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
+			if err != nil {
+				return ctx
+			}
+			pods := &corev1.PodList{}
+			namespace := "a"
+			containerName := "a-server"
+			err = client.Resources("a").List(context.TODO(), pods)
+			if err != nil || pods.Items == nil {
+				t.Error("error while getting pods", err)
+			}
+			podName := pods.Items[0].Name
+
+			var stdout, stderr bytes.Buffer
+			command := []string{"curl", "-m", "2", "-I", "http://b-server.b:80"}
+			client.Resources().ExecInPod(context.TODO(), namespace, podName, containerName, command, &stdout, &stderr)
+
+			httpStatus := strings.Split(stdout.String(), "\n")[0]
+			if !strings.Contains(httpStatus, "200") {
+				t.Fatal("Couldn't connect to server B")
+			}
+			return ctx
+
+		}).
+		Assess("curl from C to A succeeds", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
+			if err != nil {
+				return ctx
+			}
+			namespace := "c"
+			containerName := "c-server"
+			pods := &corev1.PodList{}
+			err = client.Resources("c").List(context.TODO(), pods)
+			if err != nil || pods.Items == nil {
+				t.Error("error while getting pods", err)
+			}
+			podName := pods.Items[0].Name
+
+			var stdout, stderr bytes.Buffer
+			command := []string{"curl", "-m", "2", "-I", "http://a-server.a:80"}
+			client.Resources().ExecInPod(context.TODO(), namespace, podName, containerName, command, &stdout, &stderr)
+
+			httpStatus := strings.Split(stdout.String(), "\n")[0]
+			if !strings.Contains(httpStatus, "200") {
+				t.Fatal("Couldn't connect to server A")
+			}
+			return ctx
+		}).
+		Feature()
+
+	blockCToA := features.New("blockCToA").
+		WithLabel("suite", "netpol").
+		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
+			if err != nil {
+				return ctx
+			}
+
+			log.Print("Applying Network Policy")
+			if err := client.Resources().Create(ctx, &networkPolicy); err != nil {
+				t.Error("error while applying Network Policy", err)
+				return ctx
+			}
+
+			// This time-wait is to account for Network Policy Controller to start up, run leader election in the control plane
+			// and to apply the network policy
+			time.Sleep(1 * time.Minute)
+
+			return ctx
+
+		}).
+		Assess("curl from A to B succeeds", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
+			if err != nil {
+				return ctx
+			}
+			pods := &corev1.PodList{}
+			namespace := "a"
+			containerName := "a-server"
+			err = client.Resources("a").List(context.TODO(), pods)
+			if err != nil || pods.Items == nil {
+				t.Error("error while getting pods", err)
+			}
+			podName := pods.Items[0].Name
+
+			var stdout, stderr bytes.Buffer
+			command := []string{"curl", "-m", "2", "-I", "http://b-server.b:80"}
+			client.Resources().ExecInPod(context.TODO(), namespace, podName, containerName, command, &stdout, &stderr)
+
+			httpStatus := strings.Split(stdout.String(), "\n")[0]
+			if !strings.Contains(httpStatus, "200") {
+				t.Fatal("Couldn't connect to server B")
+			}
+			return ctx
+		}).
+		Assess("curl from C to A fails", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
+			if err != nil {
+				return ctx
+			}
+			namespace := "c"
+			containerName := "c-server"
+			pods := &corev1.PodList{}
+			err = client.Resources("c").List(context.TODO(), pods)
+			if err != nil || pods.Items == nil {
+				t.Error("error while getting pods", err)
+			}
+			podName := pods.Items[0].Name
+
+			var stdout, stderr bytes.Buffer
+			command := []string{"curl", "-m", "2", "-I", "http://a-server.a:80"}
+			client.Resources().ExecInPod(context.TODO(), namespace, podName, containerName, command, &stdout, &stderr)
+
+			httpStatus := strings.Split(stdout.String(), "\n")[0]
+			if strings.Contains(httpStatus, "200") {
+				t.Fatal("Network Policy didn't block connection to server A")
+			}
+			return ctx
+		}).
+		Teardown(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
+			if err != nil {
+				return ctx
+			}
+
+			if err := client.Resources().Delete(ctx, &networkPolicy); err != nil {
+				t.Error("error while deleting Network Policy", err)
+				return ctx
+			}
+			return ctx
+		}).
+		Feature()
+
+	testenv.Test(t, allowAll, blockCToA)
+}
