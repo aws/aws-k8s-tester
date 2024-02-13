ac7b7aff890d91d39a630f62b0909f45c4f9586a
Vela WU <50354807+wwvela@users.noreply.github.com>
2024-02-13 11:51:29 -0800

    Add e2e test for user limits (#420)

---
+++ e2e2/test/cases/quick/main_test.go
@@ -0,0 +1,16 @@
+package quick
+
+import (
+	"os"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"testing"
+)
+
+var (
+	testenv env.Environment
+)
+
+func TestMain(m *testing.M) {
+	testenv = env.New()
+	os.Exit(testenv.Run(m))
+}
