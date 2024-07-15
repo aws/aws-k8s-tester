6df845be9e0f14a93ef2623a55d9c1d6f5a3c18e
Matt Johnson <mattcjo@amazon.com>
2024-07-15 21:50:05 +0000

    Add base of main_test.go for inference test

---
+++ e2e2/test/cases/bert-inference/nvidia/main_test.go
@@ -0,0 +1,84 @@
+package nvidia
+
+import (
+	"context"
+	_ "embed"
+	"flag"
+	"log"
+	"os"
+	"slices"
+	"testing"
+	"time"
+
+	fwext "github.com/aws/aws-k8s-tester/e2e2/internal/framework_extensions"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+var (
+	testenv       env.Environment
+	nodeType      *string
+	efaEnabled    *bool
+	ncclTestImage *string
+	nodeCount     int
+	gpuPerNode    int
+	efaPerNode    int
+)
+
+var (
+	//go:embed manifests/nvidia-device-plugin.yaml
+	nvidiaDevicePluginManifest []byte
+)
+
+func TestMain(m *testing.M) {
+	nodeType = flag.String("nodeType", "", "node type for the tests")
+	ncclTestImage = flag.String("ncclTestImage", "", "nccl test image for nccl tests")
+	efaEnabled = flag.Bool("efaEnabled", false, "enable efa tests")
+	cfg, err := envconf.NewFromFlags()
+	if err != nil {
+		log.Fatalf("failed to initialize test environment: %v", err)
+	}
+	testenv = env.NewWithConfig(cfg)
+
+	// all NVIDIA tests require the device plugin and MPI operator
+	manifests := [][]byte{
+		nvidiaDevicePluginManifest,
+	}
+
+	testenv.Setup(
+		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			err := fwext.ApplyManifests(config.Client().RESTConfig(), manifests...)
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			ds := appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{Name: "nvidia-device-plugin-daemonset", Namespace: "kube-system"},
+			}
+			err := wait.For(fwext.NewConditionExtension(config.Client().Resources()).DaemonSetReady(&ds),
+				wait.WithTimeout(time.Minute*5))
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+	)
+
+	testenv.Finish(
+		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			slices.Reverse(manifests)
+			err = fwext.DeleteManifests(config.Client().RESTConfig(), manifests...)
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+	)
+
+	os.Exit(testenv.Run(m))
+}
