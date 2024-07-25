c0b78b54a67ab345e3bb27a62bef5f01b13125d9
Matt Johnson <mattcjo@amazon.com>
2024-07-25 22:26:26 +0000

    Rename directory from bert-training to just training

---
+++ e2e2/test/cases/training/bert_training_test.go
@@ -0,0 +1,75 @@
+package training
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"testing"
+	"time"
+
+	fwext "github.com/aws/aws-k8s-tester/e2e2/internal/framework_extensions"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+
+	batchv1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	//go:embed manifests/bert-training.yaml
+	bertTrainingManifest         []byte
+	renderedBertTrainingManifest []byte
+)
+
+type bertTrainingManifestTplVars struct {
+	BertTrainingImage string
+	TrainingMode      string
+}
+
+func TestBertTraining(t *testing.T) {
+	bertTraining := features.New("bert-training").
+		WithLabel("suite", "nvidia").
+		WithLabel("hardware", "gpu").
+		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			if *bertTrainingImage == "" {
+				t.Fatal(fmt.Errorf("bertTrainingImage must be set to run the test"))
+			}
+
+			var err error
+			renderedBertTrainingManifest, err = fwext.RenderManifests(bertTrainingManifest, bertTrainingManifestTplVars{
+				BertTrainingImage: *bertTrainingImage,
+				TrainingMode:      *trainingMode,
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = fwext.ApplyManifests(cfg.Client().RESTConfig(), renderedBertTrainingManifest)
+			if err != nil {
+				t.Fatal(err)
+			}
+			return ctx
+		}).
+		Assess("BERT training Job succeeds", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			job := &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{Name: "bert-training", Namespace: "default"},
+			}
+			err := wait.For(fwext.NewConditionExtension(cfg.Client().Resources()).JobSucceeded(job),
+				wait.WithTimeout(time.Minute*20))
+			if err != nil {
+				t.Fatal(err)
+			}
+			return ctx
+		}).
+		Teardown(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			// Delete the manifest
+			err := fwext.DeleteManifests(cfg.Client().RESTConfig(), renderedBertTrainingManifest)
+			if err != nil {
+				t.Fatal(err)
+			}
+			return ctx
+		}).
+		Feature()
+
+	testenv.Test(t, bertTraining)
+}
---
+++ e2e2/test/cases/training/main_test.go
@@ -0,0 +1,95 @@
+package training
+
+import (
+	"context"
+	_ "embed"
+	"flag"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	fwext "github.com/aws/aws-k8s-tester/e2e2/internal/framework_extensions"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+var (
+	testenv env.Environment
+	// nodeType          *string
+	bertTrainingImage *string
+	// nodeCount         int
+	// gpuPerNode        int
+)
+
+var (
+	//go:embed manifests/nvidia-device-plugin.yaml
+	nvidiaDevicePluginManifest []byte
+	//go:embed manifests/mpi-operator.yaml
+	mpiOperatorManifest []byte
+)
+
+func TestMain(m *testing.M) {
+	bertTrainingImage = flag.String("bertTrainingImage", "", "Docker image used for BERT training workload")
+	cfg, err := envconf.NewFromFlags()
+	if err != nil {
+		log.Fatalf("failed to initialize test environment: %v", err)
+	}
+	testenv = env.NewWithConfig(cfg)
+
+	// all NVIDIA tests require the device plugin and MPI operator
+	manifests := [][]byte{
+		nvidiaDevicePluginManifest,
+		mpiOperatorManifest,
+	}
+
+	testenv.Setup(
+		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			err := fwext.ApplyManifests(config.Client().RESTConfig(), manifests...)
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			dep := appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{Name: "mpi-operator", Namespace: "mpi-operator"},
+			}
+			err := wait.For(conditions.New(config.Client().Resources()).DeploymentConditionMatch(&dep, appsv1.DeploymentAvailable, v1.ConditionTrue),
+				wait.WithTimeout(time.Minute*5))
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			ds := appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{Name: "nvidia-device-plugin-daemonset", Namespace: "kube-system"},
+			}
+			err := wait.For(fwext.NewConditionExtension(config.Client().Resources()).DaemonSetReady(&ds),
+				wait.WithTimeout(time.Minute*5))
+			if err != nil {
+				return ctx, err
+			}
+			return ctx, nil
+		},
+	)
+
+	// testenv.Finish(
+	// 	func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+	// 		slices.Reverse(manifests)
+	// 		err = fwext.DeleteManifests(config.Client().RESTConfig(), manifests...)
+	// 		if err != nil {
+	// 			return ctx, err
+	// 		}
+	// 		return ctx, nil
+	// 	},
+	// )
+
+	os.Exit(testenv.Run(m))
+}
