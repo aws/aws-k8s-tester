8231e5a03e69237d00f783acc78a25492042cf07
Pavani Panakanti <pavanipt@amazon.com>
2024-10-25 19:56:56 +0000

    nccom test

--- e2e2/test/cases/neuron/main_test.go
+++ e2e2/test/cases/neuron/main_test.go
@@ -20,15 +20,24 @@ import (
 
 var (
 	testenv             env.Environment
+	nodeType            *string
+	efaEnabled          *bool
+	nodeCount           int
+	neuronPerNode       int
+	efaPerNode          int
 	neuronTestImage     *string
 	installDevicePlugin *bool
 )
 
 var (
 	//go:embed manifests/k8s-neuron-device-plugin-rbac.yml
-	neuronDevicePlugiRbacManifest []byte
+	neuronDevicePluginRbacManifest []byte
 	//go:embed manifests/k8s-neuron-device-plugin.yml
 	neuronDevicePluginManifest []byte
+	//go:embed manifests/mpi-operator.yaml
+	mpiOperatorManifest []byte
+	//go:embed manifests/efa-device-plugin.yaml
+	efaDevicePluginManifest []byte
 )
 
 func deployNeuronDevicePlugin(ctx context.Context, config *envconf.Config) (context.Context, error) {
@@ -43,7 +52,83 @@ func deployNeuronDevicePlugin(ctx context.Context, config *envconf.Config) (cont
 	return ctx, nil
 }
 
+func deployMPIOperator(ctx context.Context, config *envconf.Config) (context.Context, error) {
+	dep := appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{Name: "mpi-operator", Namespace: "mpi-operator"},
+	}
+	err := wait.For(conditions.New(config.Client().Resources()).DeploymentConditionMatch(&dep, appsv1.DeploymentAvailable, v1.ConditionTrue),
+		wait.WithContext(ctx))
+	if err != nil {
+		return ctx, fmt.Errorf("failed to deploy mpi-operator: %v", err)
+	}
+	return ctx, nil
+}
+
+func deployEFAPlugin(ctx context.Context, config *envconf.Config) (context.Context, error) {
+	err := fwext.ApplyManifests(config.Client().RESTConfig(), efaDevicePluginManifest)
+	if err != nil {
+		return ctx, err
+	}
+
+	ds := appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{Name: "aws-efa-k8s-device-plugin-daemonset", Namespace: "kube-system"},
+	}
+	err = wait.For(fwext.NewConditionExtension(config.Client().Resources()).DaemonSetReady(&ds),
+		wait.WithContext(ctx))
+	if err != nil {
+		return ctx, fmt.Errorf("failed to deploy efa-device-plugin: %v", err)
+	}
+
+	return ctx, nil
+}
+
+func checkNodeTypes(ctx context.Context, config *envconf.Config) (context.Context, error) {
+	clientset, err := kubernetes.NewForConfig(config.Client().RESTConfig())
+	if err != nil {
+		return ctx, err
+	}
+
+	nodes, err := clientset.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return ctx, err
+	}
+
+	singleNodeType := true
+	for i := 1; i < len(nodes.Items)-1; i++ {
+		if nodes.Items[i].Labels["node.kubernetes.io/instance-type"] != nodes.Items[i-1].Labels["node.kubernetes.io/instance-type"] {
+			singleNodeType = false
+		}
+	}
+	if !singleNodeType {
+		return ctx, fmt.Errorf("Node types are not the same, all node types must be the same in the cluster")
+	}
+
+	if *nodeType != "" {
+		for _, v := range nodes.Items {
+			if v.Labels["node.kubernetes.io/instance-type"] == *nodeType {
+				nodeCount++
+				neuron := v.Status.Capacity["aws.amazon.com/neuron"]
+				neuronPerNode = int(neuron.Value())
+				efa := v.Status.Capacity["vpc.amazonaws.com/efa"]
+				efaPerNode = int(efa.Value())
+			}
+		}
+	} else {
+		log.Printf("No node type specified. Using the node type %s in the node groups.", nodes.Items[0].Labels["node.kubernetes.io/instance-type"])
+		nodeType = aws.String(nodes.Items[0].Labels["node.kubernetes.io/instance-type"])
+		nodeCount = len(nodes.Items)
+		neuron := nodes.Items[0].Status.Capacity["aws.amazon.com/neuron"]
+		neuronPerNode = int(gpu.Value())
+		efa := nodes.Items[0].Status.Capacity["vpc.amazonaws.com/efa"]
+		efaPerNode = int(efa.Value())
+	}
+
+	return ctx, nil
+}
+
 func TestMain(m *testing.M) {
+	nodeType = flag.String("nodeType", "", "node type for the tests")
+	efaEnabled = flag.Bool("efaEnabled", false, "enable efa tests")
 	neuronTestImage = flag.String("neuronTestImage", "", "image for neuron single node test")
 	installDevicePlugin = flag.Bool("installDevicePlugin", true, "install neuron device plugin")
 	cfg, err := envconf.NewFromFlags()
@@ -55,7 +140,9 @@ func TestMain(m *testing.M) {
 	defer cancel()
 	testenv = testenv.WithContext(ctx)
 
-	var manifests [][]byte
+	manifests := [][]byte{
+		mpiOperatorManifest,
+	}
 	setUpFunctions := []env.Func{
 		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
 			err := fwext.ApplyManifests(config.Client().RESTConfig(), manifests...)
@@ -64,6 +151,8 @@ func TestMain(m *testing.M) {
 			}
 			return ctx, nil
 		},
+		deployMPIOperator,
+		checkNodeTypes,
 	}
 
 	if *installDevicePlugin {
@@ -71,10 +160,18 @@ func TestMain(m *testing.M) {
 		setUpFunctions = append(setUpFunctions, deployNeuronDevicePlugin)
 	}
 
+	if *efaEnabled {
+		setUpFunctions = append(setUpFunctions, deployEFAPlugin)
+	}
+
 	testenv.Setup(setUpFunctions...)
 
 	testenv.Finish(
 		func(ctx context.Context, config *envconf.Config) (context.Context, error) {
+			err := fwext.DeleteManifests(cfg.Client().RESTConfig(), efaDevicePluginManifest)
+			if err != nil {
+				return ctx, err
+			}
 			slices.Reverse(manifests)
 			err = fwext.DeleteManifests(config.Client().RESTConfig(), manifests...)
 			if err != nil {
--- e2e2/test/cases/neuron/neuron_test.go
+++ e2e2/test/cases/neuron/neuron_test.go
@@ -18,17 +18,28 @@ import (
 var (
 	//go:embed manifests/single-node-test-neuronx.yaml
 	neuronSingleNodeManifest         []byte
+	//go:embed manifests/multi-node-test-neuron.yaml
+	neuronMultiNodeManifest          []byte
 	renderedNeuronSingleNodeManifest []byte
+	renderedNeuronMultiNodeManifest  []byte
 )
 
 type neuronSingleNodeManifestTplVars struct {
 	NeuronTestImage string
 }
 
-func TestMPIJobPytorchTraining(t *testing.T) {
+type neuronMultiNodeTestManifestTplVars struct {
+	WorkerNodeCount        int
+	WorkerNodeNeuronCount  int
+	NeuronPerNode          int
+	NeuronTestImage        string
+	EfaInterfacePerNode    int
+}
+
+func TestNeuronNodes(t *testing.T) {
 	singleNode := features.New("single-node").
 		WithLabel("suite", "neuron").
-		WithLabel("hardware", "gpu").
+		WithLabel("hardware", "neuron").
 		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			if *neuronTestImage == "" {
 				t.Fatal(fmt.Errorf("neuronTestImage must be set to run neuron single node test, use https://github.com/aws/aws-k8s-tester/blob/main/e2e2/test/images/neuron/Dockerfile to build the image and -neuronTestImage to set the image url"))
@@ -74,5 +85,80 @@ func TestMPIJobPytorchTraining(t *testing.T) {
 		}).
 		Feature()
 
+		multiNode := features.New("multi-node").
+		WithLabel("suite", "neuron").
+		WithLabel("hardware", "neuron").
+		WithLabel("hardware", "efa").
+		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			if *neuronTestImage == "" {
+				t.Fatal(fmt.Errorf("neuronTestImage must be set to run unit test, use https://github.com/aws/aws-k8s-tester/blob/main/e2e2/test/images/neuron/Dockerfile to build the image and -neuronTestImage to set the image url"))
+			}
+			renderedNeuronMultiNodeManifest, err := fwext.RenderManifests(neuronMultiNodeManifest, neuronMultiNodeTestManifestTplVars{
+				// one of the nodes will be used for the master pod
+				WorkerNodeCount:     	nodeCount,
+				WorkerNodeNeuronCount:  nodeCount * neuronPerNode,
+				NeuronPerNode:          neuronPerNode,
+				NeuronTestImage:     	*neuronTestImage,
+				EfaInterfacePerNode: 	efaPerNode,
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = fwext.ApplyManifests(cfg.Client().RESTConfig(), renderedNeuronMultiNodeManifest)
+			if err != nil {
+				t.Fatal(err)
+			}
+			return ctx
+		}).
+		Assess("NCCOM test succeeds", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			rsrc := cfg.Client().Resources()
+			if err := kubeflowv2beta1.AddToScheme(rsrc.GetScheme()); err != nil {
+				t.Fatal(err)
+			}
+			j := kubeflowv2beta1.MPIJob{
+				ObjectMeta: metav1.ObjectMeta{Name: "multi-node-nccom-test", Namespace: "default"},
+			}
+			err := wait.For(conditions.New(rsrc).ResourceMatch(&j, mpiJobRunning),
+				wait.WithContext(ctx))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Verify GPU Direct RDMA is used on P4/P5
+			log, err := fwext.GetJobLogs(cfg.Client().RESTConfig(), &kubeflowv2beta1.MPIJob{
+				ObjectMeta: metav1.ObjectMeta{Name: "multi-node-nccl-test", Namespace: "default"},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Log("Test log for multi-node-nccl-test:")
+			t.Log(log)
+			if *efaEnabled && slices.Contains(instanceSupportsRdmaRead, *nodeType) {
+				pattern := regexp.MustCompile(`\[send\] via NET/.*Libfabric/.*/GDRDMA`)
+				if !pattern.MatchString(log) {
+					t.Fatalf("GPU Direct RDMA is not utilized for inter-node communication in NCCL tests on instances that support GDRDMA: %s", *nodeType)
+				}
+			}
+			return ctx
+		}).
+		Teardown(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			err := fwext.DeleteManifests(cfg.Client().RESTConfig(), renderedMpiJobNcclTestMultiNodeManifest)
+			if err != nil {
+				t.Fatal(err)
+			}
+			return ctx
+		}).
+		Feature()
+
 	testenv.Test(t, singleNode)
+}
+
+func mpiJobRunning(obj k8s.Object) bool {
+	j := obj.(*kubeflowv2beta1.MPIJob)
+	for _, c := range j.Status.Conditions {
+		if c.Type == kubeflowv2beta1.JobRunning {
+			return c.Status == v1.ConditionTrue
+		}
+	}
+	return false
 }
