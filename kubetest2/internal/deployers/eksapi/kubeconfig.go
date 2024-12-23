564ee3c9901753078759272a0993e9f097a84d50
Vela Wu <wwvela@amazon.com>
2024-12-23 03:09:58 +0000

    Revert "handle the nil pointer exception in writeKubeconfig when no cluster resrouces"
    This reverts commit 7942571ce553d80ee0a5795c5fe84090ebef71b4.

--- kubetest2/internal/deployers/eksapi/kubeconfig.go
+++ kubetest2/internal/deployers/eksapi/kubeconfig.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"os"
 	"text/template"
-	"fmt"
 
 	"k8s.io/klog"
 )
@@ -47,9 +46,6 @@ type kubeconfigTemplateParameters struct {
 }
 
 func writeKubeconfig(cluster *Cluster, kubeconfigPath string) error {
-	if cluster == nil {
-		return fmt.Errorf("Cluster is nil, you might need set --static-cluster-name or set --up to initial cluster resrouces")
-	}
 	klog.Infof("writing kubeconfig to %s for cluster: %s", kubeconfigPath, cluster.arn)
 	templateParams := kubeconfigTemplateParameters{
 		ClusterCertificateAuthority: cluster.certificateAuthorityData,
