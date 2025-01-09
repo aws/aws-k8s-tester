5a9f53253b6e8838f8ec893a8d80b172a96e27e5
Carter McKinnon <mckdev@amazon.com>
2025-01-08 19:29:17 -0800

    Add parallelism to janitor, stack status filter

--- cmd/kubetest2-eksapi-janitor/main.go
+++ cmd/kubetest2-eksapi-janitor/main.go
@@ -12,10 +12,14 @@ import (
 func main() {
 	var maxResourceAge time.Duration
 	flag.DurationVar(&maxResourceAge, "max-resource-age", time.Hour*3, "Maximum resource age")
+	var workers int
+	flag.IntVar(&workers, "workers", 1, "number of workers to processes resources in parallel")
+	var stackStatus string
+	flag.StringVar(&stackStatus, "stack-status", "", "only process stacks with a specific status")
 	var emitMetrics bool
 	flag.BoolVar(&emitMetrics, "emit-metrics", false, "Send metrics to CloudWatch")
 	flag.Parse()
-	j := eksapi.NewJanitor(maxResourceAge, emitMetrics)
+	j := eksapi.NewJanitor(maxResourceAge, emitMetrics, workers, stackStatus)
 	if err := j.Sweep(context.Background()); err != nil {
 		klog.Fatalf("failed to sweep resources: %v", err)
 	}
