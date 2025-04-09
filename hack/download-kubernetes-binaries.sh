a09674a4ed8a2bd63bb10cde63f634f8deb7dee1
Nick Baker <nbakerd@amazon.com>
2025-04-09 13:27:08 -0700

    build: fallback to release ci dev for kuberentes binaries (#602)

--- hack/download-kubernetes-binaries.sh
+++ hack/download-kubernetes-binaries.sh
@@ -24,16 +24,24 @@ echo >&2 "Release marker: ${RELEASE_MARKER}"
 OS="$2"
 ARCH="$3"
 
-KUBERNETES_VERSION=$(curl --silent "https://storage.googleapis.com/kubernetes-release/release/${RELEASE_MARKER}")
+function download_binaries() {
+  local basePath=$1
 
-echo "Kubernetes version: ${KUBERNETES_VERSION}"
-echo "${KUBERNETES_VERSION}" > kubernetes-version.txt
+  local KUBERNETES_VERSION=$(curl --silent "${basePath}/${RELEASE_MARKER}")
 
-for BUNDLE in ${BUNDLES[@]}; do
-  echo >&2 "Downloading bundle: ${BUNDLE}"
-  TARBALL="${BUNDLE}.tar.gz"
-  wget --quiet --output-document=${TARBALL} https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_VERSION}/${BUNDLE}-${OS}-${ARCH}.tar.gz
-  tar xzf ${TARBALL}
-  rm ${TARBALL}
-done
+  echo "Kubernetes version: ${KUBERNETES_VERSION}"
+  echo "${KUBERNETES_VERSION}" > kubernetes-version.txt
 
+  for BUNDLE in ${BUNDLES[@]}; do
+    echo >&2 "Downloading bundle: ${BUNDLE}"
+    local TARBALL="${BUNDLE}.tar.gz"
+    wget --quiet --output-document=${TARBALL} $basePath/${KUBERNETES_VERSION}/${BUNDLE}-${OS}-${ARCH}.tar.gz
+    tar xzf ${TARBALL}
+    rm ${TARBALL}
+  done
+}
+
+if ! download_binaries https://storage.googleapis.com/kubernetes-release/release || true; then
+  echo >&2 "binary download failed from release bucket, falling back to ci dev release"
+  download_binaries https://storage.googleapis.com/k8s-release-dev/ci
+fi
