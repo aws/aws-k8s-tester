386d5790bb62fb126066d1286fd0f176602fec7a
mselim00 <100049111+mselim00@users.noreply.github.com>
2025-06-11 09:37:28 -0700

    fix(neuron): install tenacity, upate deps, propagate failures (#629)

--- test/images/neuron/tests/singleNodeTest.sh
+++ test/images/neuron/tests/singleNodeTest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 torchrun --nproc_per_node=2 --nnodes=1 tests/testNeuronSingleAllReduce.py
 torchrun --nproc_per_node=2 --nnodes=1 tests/testNeuronParallelState.py
 torchrun --nproc_per_node=2 --nnodes=1 tests/testNeuronMlp.py
