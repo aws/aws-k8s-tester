10b40c4c91325e44a49d0d8b19e893c2bbecd754
Vela Wu <wwvela@amazon.com>
2026-09-18 18:20:10 +0000

    let janitor sweep kubetest2 stack in all available regions

--- cmd/kubetest2-eksapi-janitor/main.go
+++ cmd/kubetest2-eksapi-janitor/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-k8s-tester/internal/deployers/eksapi"
@@ -19,8 +20,16 @@ func main() {
 	flag.StringVar(&stackStatus, "stack-status", "", "only process stacks with a specific status")
 	var emitMetrics bool
 	flag.BoolVar(&emitMetrics, "emit-metrics", false, "Send metrics to CloudWatch")
+	var regions string
+	flag.StringVar(&regions, "regions", "", "comma-separated regions to sweep; when empty, all regions enabled for the account are swept")
 	flag.Parse()
-	j := eksapi.NewJanitor(maxResourceAge, emitMetrics, workers, stackStatus)
+	var regionsList []string
+	for _, r := range strings.Split(regions, ",") {
+		if r = strings.TrimSpace(r); r != "" {
+			regionsList = append(regionsList, r)
+		}
+	}
+	j := eksapi.NewJanitor(maxResourceAge, emitMetrics, workers, stackStatus, regionsList)
 	if err := j.Sweep(context.Background()); err != nil {
 		slog.Error("failed to sweep resources", "error", err)
 		os.Exit(1)
